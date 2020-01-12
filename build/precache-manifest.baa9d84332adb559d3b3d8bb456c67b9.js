23eec8504337da514a80f69f57f3a51b0df7cae7
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-01-11 19:43:12 -0500

    Check in build

---
+++ build/precache-manifest.baa9d84332adb559d3b3d8bb456c67b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce02635cd7310a41b7e1883e7ac26691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49870c96edb2d593fd16",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bc5819a2085a7a9fc08",
+    "url": "/static/js/2.1495c05f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1495c05f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49870c96edb2d593fd16",
+    "url": "/static/js/main.8dcbbcda.chunk.js"
+  },
+  {
+    "revision": "baa5202a36d073a27a5d",
+    "url": "/static/js/runtime-main.f37d2940.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
